5a66ca612e4bcb902d0ed93f159b0671f304c2ab
jennie.lindahl <jennie.lindahl@lynkco.com>
2024-02-28 15:29:51 +0100

    added MutableStateFlow

---
+++ shared/src/iosMain/kotlin/com/moriawe/translationapp/core/domain/util/CommonMutableStateFlow.kt
@@ -0,0 +1,34 @@
+package com.moriawe.translationapp.core.domain.util
+
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.StateFlow
+
+actual open class CommonMutableStateFlow<T> actual constructor(
+    private val flow: MutableStateFlow<T>
+): CommonStateFlow<T>, MutableStateFlow<T> {
+    override var value: T
+        get() = super.value
+        set(value) {
+            flow.value = value
+        }
+
+    override val subscriptionCount: StateFlow<Int>
+        get() = flow.subscriptionCount
+
+    override fun compareAndSet(expect: T, update: T): Boolean {
+        return flow.compareAndSet(expect, update)
+    }
+
+    @ExperimentalCoroutinesApi
+    override fun resetReplayCache() {
+        flow.resetReplayCache()
+    }
+
+    override fun tryEmit(value: T): Boolean {
+        return flow.tryEmit(value)
+    }
+
+    override suspend fun emit(value: T) {
+        flow.emit(value)
+    }
+}
