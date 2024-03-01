14a4e586168bd8e13a06a0eb4bccc37c36869bd4
Moriawe <moriawe22@gmail.com>
2024-03-01 12:50:28 +0100

    commented out code that won't work on Windows

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -50,7 +50,7 @@ kotlin {
         val iosArm64Main by getting
         val iosSimulatorArm64Main by getting
         val iosMain by creating {
-            dependsOn(commonMain.get())
+            //dependsOn(commonMain.get())
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
@@ -64,7 +64,7 @@ kotlin {
         val iosArm64Test by getting
         val iosSimulatorArm64Test by getting
         val iosTest by creating {
-            dependsOn(commonTest.get())
+            //dependsOn(commonTest.get())
             iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
             iosSimulatorArm64Test.dependsOn(this)
